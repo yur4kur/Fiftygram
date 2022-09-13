e1a5e9e2fbdc4ff09069d6e37e449c43c6bfe011
Iurii Kurinnoi <yur4kur@icloud.com>
2022-09-13 17:26:45 +0400

    Initial Commit

---
+++ Fiftygram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fiftygram
+//
+//  Created by Юрий Куринной on 13.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
